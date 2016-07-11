0d0b9ef646a44daaa845db87921d7b5305a4684e
Dwight Spencer <denzuko@users.sf.net>
2016-07-11 15:13:02 +0000

    (feat) Regression test - Google.com

---
+++ spec/google.js
@@ -0,0 +1,19 @@
+var request   = require('supertest'),
+    should    = require('should');
+
+describe('Ensure google is online', function () {
+    
+  var base = "http://www.google.com/";
+
+  it('responds with HTTP status 200 from GET /', function testSlash(done) {
+     request(base).get('/')
+       .expect(200)
+       .end(done);
+  });
+  
+  it('Content-Type of plain/html from GET /', function testContentType(done) {
+    request(base).get('/')
+      .expect('Content-Type', /html/)
+      .end(done)
+  });
+});
